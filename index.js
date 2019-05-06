482a15904e2c2f68578cdca1f1323d75e784d3a9
alexandergf <alexgferreiro@hotmail.com>
2019-05-06 08:58:44 +0200

    v0.64

--- index.js
+++ index.js
@@ -18,6 +18,7 @@ import UnirseGrupo from './src/screens/UnirseGrupo';
 import Mapa from './src/screens/Mapa';
 import PuntoEncuentro from './src/screens/PuntoEncuentro';
 import GestionarMiembros from './src/screens/GestionarMiembros';
+import foto from './src/screens/foto';
 
 
 const AppNavigator = createStackNavigator(
