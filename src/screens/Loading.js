e38c00c7c448435af51a1861904e74406f013a26
alexandergf <alexgferreiro@hotmail.com>
2019-05-14 12:48:40 +0200

    Loading perfeccionado

--- src/screens/Loading.js
+++ src/screens/Loading.js
@@ -3,13 +3,39 @@ import { View, Text, StyleSheet, TextInput, TouchableHighlight, ImageBackground,
 import firebase from 'firebase';  
 import { db } from '../config';
 import { Navigation } from 'react-navigation';
+let userRef = db.ref('/Users');
 export default class Loading extends Component {  
+  constructor(){
+    super();
+    this.state = {
+      items: []
+    }
+  }
   componentDidMount() {
+    
     firebase.auth().onAuthStateChanged(user => {
-      this.props.navigation.replace(user==null ? 'Home' : 'HomeLogIn')
+      //this.props.navigation.replace(user==null ? 'Home' : 'HomeLogIn')
+      if(user == null){
+        this.props.navigation.replace('Home')
+      } else {
+        userRef.on('value', snapshot => {
+          let data = snapshot.val();
+          let items = Object.values(data);
+          var look = false;
+          items.map((item, index) => {
+            if (user.email == item.email && item.active == true){
+              look=true;
+              this.props.navigation.replace('Mapa');
+            } 
+          })
+          if (look == false){
+            this.props.navigation.replace('HomeLogIn');
+          }
+        });
+      }
     })
   }   
- 
+
   render() {
     return (
       <ImageBackground source={require('../images/fondo.png')} style={styles.container}>
